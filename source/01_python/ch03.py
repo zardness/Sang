3b3c7b1ffd567bb95c0f0abc0cd293e9d0c13f19
zardness <zardless@naver.com>
2026-06-26 17:59:58 +0900

    데이터 구조

---
+++ source/01_python/ch03.py
@@ -0,0 +1,20 @@
+def safe_index(lst,item):
+    '''
+    첫번째 매개변수 lst에서 item 요소가 있는 index를 변환하는 함수
+    item 요소가 없으면 -1을 반환
+    '''
+    if item in lst:
+        return lst.index(item)
+    return-1
+
+
+
+def safe_remove(lst, remove_item):
+    '''
+    첫번째 매개변수 lst에서 remove_item 데이터 제거
+    해당 remove_item이 lst에 없으면 메세지 출력
+    '''
+    if remove_item in lst:
+        lst.remove(remove_item)
+    else:
+        print('해당 데이터는 없는 데이터입니다.')
