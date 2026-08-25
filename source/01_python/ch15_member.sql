4a8bd7b9610ad14a9092e10c21feb39eabf0beb7
zardness <zardless@naver.com>
2026-08-25 18:01:24 +0900

    머신러닝학습전후비교

---
+++ source/01_python/ch15_member.sql
@@ -0,0 +1,30 @@
+--회원가입 | 전체조회 | 이름찾기 | 메일삭제 | csv내보내기 
+DROP TABLE MEMBER;
+CREATE TABLE MEMBER(
+    NAME VARCHAR2(20),
+    PHONE VARCHAR2(30),
+    EMAIL VARCHAR2(50),
+    AGE NUMBER(3),
+    GRADE NUMBER(1)   
+);    
+SELECT * FROM MEMBER
+
+-- 1.회원가입 
+INSERT INTO MEMBER VALUES ('홍길동','010-9999-9999','SH@j.com',25,1);
+INSERT INTO MEMBER VALUES ('홍길동','010-7777-9999','abc@j.com',33,1);
+-- 2. 전체조회 /5.csv내보내기
+SELECT NAME , PHONE , EMAIL , AGE , GRADE  FROM MEMBER;
+-- 3. 이름으로 찾기
+SELECT * FROM MEMBER WHERE NAME = '홍길동';
+-- 4. 메일로 삭제
+DELETE FROM MEMBER WHERE upper (EMAIL)=upper('SH@j.com');
+commit;
+
+
+
+
+
+
+
+
+
